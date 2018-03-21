e9ae165f385b37fc90ed8c6a1de8b59fa2012a1a
Oleg Filth <trust.nobody.obodin@gmail.com>
2018-03-21 17:17:36 +0300

    Add photoswipe and modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 //Vendors
 window.jQuery = window.$ = require('../gulp-dev/node_modules/jquery');
 //require ('../gulp-dev/node_modules/popper.js');
-//require ('../gulp-dev/node_modules/bootstrap');
+require ('../gulp-dev/node_modules/bootstrap');
 window.Swiper = require ('../gulp-dev/node_modules/swiper');
 window.TimeLite = require ('../gulp-dev/node_modules/gsap');
 window.ScrollMagic = require ('../gulp-dev/node_modules/scrollmagic');
 require('../gulp-dev/node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+
+window.PhotoSwipe = require ('../gulp-dev/node_modules/photoswipe');
+window.PhotoSwipeUI_Default = require ('../gulp-dev/node_modules/photoswipe/dist/photoswipe-ui-default');
