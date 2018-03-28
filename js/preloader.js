f3db991f5ee05802af8a8512a86eade54ea8b396
Oleg Filth <trust.nobody.obodin@gmail.com>
2018-03-28 16:59:15 +0300

    Fix preloader

--- js/preloader.js
+++ js/preloader.js
@@ -4,10 +4,15 @@ var preloader = document.querySelector('.preloader');
 var body = document.querySelector('body');
 
 var hidePreloader = () => {
+  
   setTimeout(()=>{
     preloader.classList.add('fadeOut', 'animated');
     body.classList.remove('js-preloader');
   }, 1500)
+ 
+  setTimeout(()=>{
+    preloader.style.zIndex = '-1';
+  }, 2500)
 };
 
 window.onload = hidePreloader();
